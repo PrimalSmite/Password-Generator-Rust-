5626e45c4b2d187e5a255e9ee73dc06fd6d7377a
Artemy <mistetartemf@gmail.com>
2024-03-12 00:32:37 +0400

    Рабочие функции: generate, transform, ask_len

--- src/funcs.rs
+++ src/funcs.rs
@@ -1,28 +1,39 @@
-use rand::Rng;
-use std::io;
-
 pub mod password{
-    const SYMBOLS: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    use std::io;
+    use rand::Rng;
+
+    pub const SYMBOLS: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~";
-        
-    let mut rng = rand::thread_rng();
-    
-    fn ask(){
-        println!("Введите количество символов для пароля:");
-        let mut count:i32;
-        match io::stdin().read_line(&mut count){
-            Ok() => {
-                return count;
-            },
-            Err(e) => {
-                println!("Ошибка - {}",e);
-            }
-        }
 
+    pub fn transform(string: &str) -> u8 {
+        let number:u8 = string.trim().parse().expect("Не удалось распознать число. Пожалуйста, убедитесь, что вы ввели число от 0 до 255");
+
+        number
+    } 
+
+    pub fn ask_len() -> u8 {
+        println!("Введите количество символов в пароле:");
+        let mut _count = String::new();
+        io::stdin().read_line(&mut _count).expect("error");
+        let len_int: u8 = transform(&_count);
+
+        len_int
     }
 
-    pub fn generate(count: i32, symbols: String){
-        ask();  
+    pub fn generate(symbols: &[u8]) -> String{
+        let mut rng = rand::thread_rng();
+       
+        let lenght: u8 = ask_len(); 
+
+        let password: String = (0..lenght)
+            .map(|_| {
+                let index = rng.gen_range(0..SYMBOLS.len());
+                SYMBOLS[index] as char
+            })
+            .collect();
+        println!("{:?}", password);
+
+        password
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
 mod funcs;
+use std::io;
+pub use crate::funcs::password;
 
-fn main() {
-    println!("Hello, world!");
+/*
+let mut action = String::new();
+fn menu() -> u8{
+    println!("Выберите действие:
+             (1) Создать пароль
+             (2) Сохранить пароль
+             (0) Выход");
 
-    funcs::password::generate();
+    //let mut action = String::new();
+    io::stdin().read_line(&mut action).expect("error");
+    let _act_int: u8 = password::transform(&action);
+    _act_int
+}
+*/
+fn main(){
+    let act_str = String::new();
+    let act: u8;
+    fn menu(string: &String) -> &u8{
+        println!("Выберите действие:
+        (1) Создать пароль
+        (2) Сохранить пароль
+        (0) Выход");
+
+        io::stdin().read_line(&mut string).expect("Не удалось ввести данные");
+
+    }
+
+    while menu_act != 0 {
+       if menu_act == 1 {
+           password::generate(password::SYMBOLS);
+           println!("Хотите сохранить пароль?
+                    (1) Да
+                    (2) Нет");
+           let mut save = String::new();
+           io::stdin().read_line(&mut save).expect("Не удалось ввести данные");
+           let save_int:u8 = password::transform(&save);
+           
+           break; 
+       }
+    }
 }
