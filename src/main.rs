86cccbbca1e009f8bdb53c189559542259fc75fa
Artemy <mistetartemf@gmail.com>
2024-03-13 21:45:33 +0400

    Working module 'menu'

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,53 @@
 mod funcs;
-use std::io;
+pub use std::io;
 pub use crate::funcs::password;
 
-/*
-let mut action = String::new();
-fn menu() -> u8{
-    println!("Выберите действие:
-             (1) Создать пароль
-             (2) Сохранить пароль
-             (0) Выход");
+mod menu{
+    use std::io;
+    use crate::password;
 
-    //let mut action = String::new();
-    io::stdin().read_line(&mut action).expect("error");
-    let _act_int: u8 = password::transform(&action);
-    _act_int
-}
-*/
-fn main(){
-    let act_str = String::new();
-    let act: u8;
-    fn menu(string: &String) -> &u8{
+    pub fn menu() -> u8{
         println!("Выберите действие:
         (1) Создать пароль
         (2) Сохранить пароль
         (0) Выход");
 
-        io::stdin().read_line(&mut string).expect("Не удалось ввести данные");
 
+        //Ввод числа
+        let mut menu_act = String::new();
+        io::stdin().read_line(&mut menu_act).expect("error");
+        let action:u8 = password::transform(&menu_act);
+        action
     }
+}
 
-    while menu_act != 0 {
-       if menu_act == 1 {
+
+fn main(){
+    let act = menu::menu();
+
+    //Основной цикл
+    while act != 0 {
+
+        //Создание пароля
+       if act == 1 {
            password::generate(password::SYMBOLS);
            println!("Хотите сохранить пароль?
                     (1) Да
                     (2) Нет");
-           let mut save = String::new();
-           io::stdin().read_line(&mut save).expect("Не удалось ввести данные");
-           let save_int:u8 = password::transform(&save);
+
+           //Ввод числа
+           let mut save_string = String::new();
+           io::stdin().read_line(&mut save_string).expect("Не удалось ввести данные");
+           let save:u8 = password::transform(&save_string);
+           
+           //Сохранение пароля
+            if save == 1{
+                
+            } else if save == 2 {
+                
+            } else {
+
+            }
            
            break; 
        }
